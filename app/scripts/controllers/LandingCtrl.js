c1f206e767bb7cdc826fcc328950e493c55048dd
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-01-30 16:24:15 -0500

    Routing established

---
+++ app/scripts/controllers/LandingCtrl.js
@@ -0,0 +1,9 @@
+(function() {
+    function LandingCtrl() {
+        this.placeHolder = "Meerkats 4 Eva!!!";
+    }
+
+    angular
+        .module('blocChat')
+        .controller('LandingCtrl', LandingCtrl);
+})();
