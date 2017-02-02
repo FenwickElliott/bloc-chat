b46d401dda6f20a2a29b0cc2be5ce35d041de44c
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-02-02 17:26:21 -0500

    modal openeing with wrong template

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -1,6 +1,16 @@
 (function(){
     function ModalCtrl(Room, $uibModal) {
         this.Room = Room;
+
+        this.open = function(){
+            $uibModal.open({
+                templateUrl: '/templates/modal.html'
+            });
+        };
+
+        this.message = "Modal scope functioning";
+
+
     };
 
     angular
