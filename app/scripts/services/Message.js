433980e25e7bf4e1262c1b22199ba0fb071fa495
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-02-03 14:13:54 -0500

    stable

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,17 +1,23 @@
 (function() {
     function Message($firebaseArray) {
         var ref = firebase.database().ref().child("rooms");
-        var reff = ref.child("-Kc4BrRJyw9FAg6Jrchw");
+        var reff = ref.child('-Kc4NAYeTuW4XXzMl59A')
         var messages = $firebaseArray(reff);
         var Message = {};
-        Message.all = messages;
+        Message.messages = messages;
 
         Message.up = $firebaseArray(ref.child("-Kc4BrRJyw9FAg6Jrchw"))
 
         Message.addUp = function(){
-            newName = prompt("Message Content:");
-            Message.up.$add(newName);
+            newMessage = prompt("Message Content:");
+            Message.up.$add({
+                username: "Charles",
+                content: newMessage,
+                sentAt: "<TIME MESSAGE WAS SENT HERE>",
+                roomId: "<ROOM UID HERE>"
+            });
         }
+        // Message.reff = reff;
 
         return Message;
     }
