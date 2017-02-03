9bfbe69291b393eb18dbcdb226ec882ea24e721a
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-02-03 14:24:53 -0500

    chat functioning

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,23 +1,21 @@
 (function() {
     function Message($firebaseArray) {
-        var ref = firebase.database().ref().child("rooms");
-        var reff = ref.child('-Kc4NAYeTuW4XXzMl59A')
-        var messages = $firebaseArray(reff);
         var Message = {};
+        Message.roomId = "-Kc4NAYeTuW4XXzMl59A";
+
+        var ref = firebase.database().ref().child("messages");
+        var messages = $firebaseArray(ref);
         Message.messages = messages;
 
-        Message.up = $firebaseArray(ref.child("-Kc4BrRJyw9FAg6Jrchw"))
-
-        Message.addUp = function(){
+        Message.add = function(){
             newMessage = prompt("Message Content:");
-            Message.up.$add({
+            Message.messages.$add({
                 username: "Charles",
                 content: newMessage,
-                sentAt: "<TIME MESSAGE WAS SENT HERE>",
-                roomId: "<ROOM UID HERE>"
+                sentAt: "noon",
+                roomId: Message.roomId
             });
         }
-        // Message.reff = reff;
 
         return Message;
     }
